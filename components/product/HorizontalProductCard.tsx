2cc8d710d49601552df37c6b124981e0dbb469e6
Vavarine <evailson.m.barbosa@gmail.com>
2024-03-30 15:38:12 -0300

    task 6

--- components/product/HorizontalProductCard.tsx
+++ components/product/HorizontalProductCard.tsx
@@ -3,13 +3,16 @@ import AddToCartButtonVTEX from "../../islands/AddToCartButton/vtex.tsx";
 import { Product } from "apps/commerce/types.ts";
 import { formatPrice } from "$site/sdk/format.ts";
 import { useOffer } from "$site/sdk/useOffer.ts";
+import { clx } from "deco-sites/camp-eva/sdk/clx.ts";
 
 interface HorizontalProductCardProps {
   product: Product;
+  zoomOnHover?: boolean;
 }
 
 export const HorizontalProductCard = ({
   product,
+  zoomOnHover,
 }: HorizontalProductCardProps) => {
   const { productID, name, url, offers, isVariantOf, image: images } = product;
 
@@ -23,19 +26,25 @@ export const HorizontalProductCard = ({
   const [image] = images ?? [];
 
   return (
-    <div class="flex border border-neutral p-2 gap-2 max-sm:flex-col max-w-xl">
+    <div class="grid grid-cols-[2fr_3fr] border border-neutral p-2 gap-2 max-sm:flex-col w-full rounded">
       {image.url && (
-        <Image
-          src={image.url}
-          width={208}
-          height={208}
-          loading="lazy"
-          alt={name}
-        />
+        <div class="aspect-square h-full overflow-hidden">
+          <Image
+            src={image.url}
+            class={clx(
+              "h-full w-full",
+              zoomOnHover && "hover:scale-110 h-full w-full transition"
+            )}
+            width={208}
+            height={208}
+            loading="lazy"
+            alt={name}
+          />
+        </div>
       )}
       <div class="px-2 flex flex-col self-stretch">
-        <h2 class="text-lg">{name}</h2>
-        <p class="mb-auto text-sm line-clamp-2">{description}</p>
+        <h2 class="text-lg line-clamp-1">{name}</h2>
+        <p class="mb-auto text-sm line-clamp-1">{description}</p>
         <div class="mb-2">
           {listPrice && (
             <p class="text-xs opacity-80 line-through">
--- sections/Product/HorizontalProductCard.tsx
+++ sections/Product/HorizontalProductCard.tsx
@@ -3,38 +3,63 @@
 import type { Product } from "apps/commerce/types.ts";
 import { HorizontalProductCard } from "$site/components/product/HorizontalProductCard.tsx";
 import { asset } from "$fresh/runtime.ts";
+import { clx } from "deco-sites/camp-eva/sdk/clx.ts";
+
+export type maxWidth =
+  | "max-w-xl"
+  | "max-w-2xl"
+  | "max-w-3xl"
+  | "max-w-4xl"
+  | "max-w-5xl"
+  | "max-w-6xl"
+  | "max-w-7xl"
+  | "max-w-full";
+
+export interface HorizontalProductCardPropsLayout {
+  larguraMax?: maxWidth;
+  zoomOnHover?: boolean;
+}
 
 export interface HorizontalProductSectionProps {
   products: Product[] | null;
+  layout: HorizontalProductCardPropsLayout;
 }
 
 export function loader(props: HorizontalProductSectionProps, _req: Request) {
-  throw new Error("Not implemented");
+  // throw new Error("Not implemented");
 
   return props;
 }
 
 const HorizontalProductSection = ({
   products,
+  layout,
 }: HorizontalProductSectionProps) => {
   if (!products?.length) return null;
 
   return (
-    <div class="container flex items-center justify-center gap-x-4 flex-wrap py-4">
+    <div
+      class={clx(
+        "container flex items-center justify-center gap-x-4 flex-wrap h-full py-4",
+        layout.larguraMax
+      )}
+    >
       {products.map((product) => (
-        <HorizontalProductCard product={product} />
+        <HorizontalProductCard
+          product={product}
+          zoomOnHover={layout.zoomOnHover}
+        />
       ))}
     </div>
   );
 };
 
 export function LoadingFallback() {
-  // Renderer spinners, skeletons and other placeholder
   return (
     <div class="container flex justify-center py-4">
-      <div class="flex max-sm:flex-col gap-4">
+      <div class="grid grid-cols-[2fr_3fr] gap-4 max-w-xl">
         <div class="skeleton h-52 w-52 shrink-0"></div>
-        <div class="px-2 flex flex-col gap-1 self-stretch shrink-0 w-64">
+        <div class="px-2 flex flex-col gap-1 ">
           <div class="skeleton h-4"></div>
           <div class="skeleton h-4 w-full mb-auto"></div>
           <div class="skeleton h-4 w-14"></div>
